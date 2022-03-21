57a1059b5c311ec2c277ef1544b65dae5542cbd9
kaiji <jgu3@mail.sfsu.edu>
2022-03-20 22:59:21 -0700

    update for classwork 7

--- back-end/src/main/java/SparkDemo.java
+++ back-end/src/main/java/SparkDemo.java
@@ -1,10 +1,53 @@
+import com.google.gson.Gson;
 import static spark.Spark.*;
+
+class InputDto{
+  String firstName;
+  String lastName;
+}
+
+class OutputDto{
+  String message;
+
+  public OutputDto(String message){
+    this.message = message;
+  }
+}
 
 public class SparkDemo {
 
   public static void main(String[] args) {
     port(1234);
+    Gson gson = new Gson();
 
     get("/hello", (req, res) -> "asd");
+
+    get("/",(req, res) -> "This is the root path");
+
+    // group relate endpoints
+    path("/api",() -> {
+      get("/a",(req, res) -> "A"); // /api/a
+      get("/b",(req, res) -> "B"); // /api/b
+
+      //queryParams part of query string (GET)
+      get("/queryTest", (req,res)->{
+        String name = req.queryParams("name");
+        if(name == null || name.isEmpty()){
+          return "Hello, please enter your name!";
+        }
+        return "Hello " + name + "!";
+      });
+
+      //body (POST or PUT)
+      post("/postTest", (req, res)->{
+        String body = req.body(); // raw text in json format
+        var inputDto = gson.fromJson(body, InputDto.class);
+        String message = "Hello "+ inputDto.firstName + " " + inputDto.lastName;
+        System.out.println(body);
+        System.out.println(message);
+        var outputDto = new OutputDto(message);
+        return gson.toJson(outputDto); //java object -> json String
+      });
+    });
   }
 }
