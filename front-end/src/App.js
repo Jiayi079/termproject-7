955baed67c7946034f6dfdb0b2fb39a48918728e
kaiji <jgu3@mail.sfsu.edu>
2022-03-20 23:00:11 -0700

    update for homework 7 - front end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  // message is a state variable, change it with setMessage
+  const [message, setMessage] = React.useState(null);
+  function handleSumbit(){
+    //alert("Clicked!");
+    console.log('click!');
+    //api call to java
+    const body = {
+      firstName: 'Jiayi',
+      lastName: 'Gu',
+    };
+    const options = {
+      method: 'post',
+      body: JSON.stringify(body),
+    };
+    fetch('/api/postTest', options)
+      .then(res => res.json()) // do this after api call finsihes
+      .then(data => {
+        console.log(data);
+        setMessage(data.message);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleSumbit}>Sumbit</button>
+      <h1>{message}</h1>
     </div>
   );
 }
